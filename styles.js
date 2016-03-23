8d94d184a83c71691d23ebf2d5beb40aafb14b05
Daw-Chih Liou <dawchih.liou@bloomon.nl>
2016-03-23 16:47:05 +0800

    add style

---
+++ styles.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+
+import React, { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  Box: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  smBox: {
+    flex: 1,
+    backgroundColor: '#3498db',
+  },
+  mdBox: {
+    flex: 2,
+    backgroundColor: '#f1c40f',
+  },
+  lgBox: {
+    flex: 3,
+    backgroundColor: '#34495e',
+  },
+  text: {
+    color: '#fff',
+  },
+});
+
+export default styles;
